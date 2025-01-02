25242c872fcb69e3e400f9dc859126100ccf3e23
Jonas Skog <jonas.skoog95@gmail.com>
2025-01-02 17:57:42 +0100

    Finished Day 02

---
+++ src/Day01/task.py
@@ -0,0 +1,5 @@
+print("Welcome to Band Name Generator")
+city = input("What's the name of the city that you grew up in? \n")
+pet_name = input("What's your pet's name? \n")
+band_name = f"{city} {pet_name}"
+print(f"Your band name could be {band_name}")

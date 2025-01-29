5a96244ce3fd9e11263b57ab747528c1a516687a
Jonas Skog <jonas.skoog95@gmail.com>
2025-01-29 21:36:17 +0100

    Finished day06

---
+++ src/Day06/task.py
@@ -0,0 +1,9 @@
+'''
+This day was all about functions and also some while loops. 
+
+Final Project was the following exercise. 
+
+Link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+
+'''
